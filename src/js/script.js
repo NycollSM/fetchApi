29587a4d189146cf102d44a07343742d6f5ca478
NycollSM <nycollsotomorales21@gmail.com>
2018-12-03 17:29:35 -0600

    for that add the songs that you click in another div

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,22 @@
-/**
- * Project Description.
- * ...
- */
+const songs = document.getElementsByTagName('audio');
+console.log(songs);
+const div = document.getElementById('contentsongs');
+let i;
+let source = i.src;
 
-const text = 'Hola mundo!';
-const container = document.getElementById('container');
-const paragraph = document.createElement('p');
-paragraph.innerText = text;
-container.appendChild(paragraph);
+// cancion al nuevo div
+function selected(){ // eslint-disable-line
+  const newSong = document.createElement('audio');
+  newSong.setAttribute('src', source);
+  newSong.setAttribute('controls', true);
+  div.appendChild(newSong);
+}
+
+// Eventos
+
+// recorre todas las canciones y con esto la funcion select va a agregar la cancion seleccionada al otro div
+for ( i of songs){
+    i.addEventListener('click' , selected);
+    source = i.src;
+    console.log(source);
+}
