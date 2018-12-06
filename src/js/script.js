64a21eb3562f11ebf16e3e385a06c8866b9170db
alexasacasa <ale.sacaza98@gmail.com>
2018-12-05 18:04:28 -0600

    Se imprimen los lyrics en la consola

--- src/js/script.js
+++ src/js/script.js
@@ -3,20 +3,6 @@ const songs = document.getElementsByTagName('audio');
 console.log(songs);
 const div = document.getElementById('contentsongs');
 
-// Fetch de lyrics.json
-const btn = document.getElementById('btn');
-
-function fetchJson() {
-  fetch('js/lyrics.json')
-    .then(res => res.json())
-    .then(data => {
-      console.log(data);
-    })
-}
-
-console.log('prueba');
-btn.addEventListener('click', fetchJson);
-
 let source;
 let i;
 
@@ -37,3 +23,42 @@ for (i of songs) {
   source = i.src;
   console.log(source);
 }
+
+// Fetch de lyrics.json
+const container = document.getElementById('container');
+const btn = document.getElementById('btn');
+
+
+/*function addLyrics() {
+  const lyrics = document.createElement('div');
+  for (const l of res.data.name) {
+    console.log(l);
+    parrafo.innerHTML += res.lyrics[l];
+  }
+  lyrics.appendChild(parrafo);
+  container.appendChild(lyrics);
+  console.log(parrafo);
+} */
+
+function fetchJson() {
+  fetch('js/lyrics.json')
+    .then(res => res.json())
+    .then(data => {
+    let respuesta = data[0].lyrics;
+      console.log(respuesta);
+      const lyrics = document.createElement('div');
+      for (const l of data.name) {
+        console.log(l);
+        const parrafo = document.createElement('p');
+        for (consta in l.lyrics) {
+          parrafo.innerHTML += res.lyrics[l];
+        }
+        lyrics.appendChild(parrafo);
+      }
+      container.appendChild(lyrics);
+      console.log(parrafo);
+      console.log(res);
+    })
+}
+
+btn.addEventListener('click', fetchJson);
